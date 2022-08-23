3cea4486e24bfe81d1e29a173ac3ec3c584eac0e
aahland <anton.ahlander@coolstuff.com>
2022-08-23 16:30:42 +0200

    WIP: resume layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,122 @@
 <script>
+	function openNav() {
+		console.log('clicked');
+		let navContainer = document.getElementById('navContainer');
+		if (navContainer) {
+			navContainer.style.visibility = 'visible';
+			console.log('this is it');
+		}
+		let textLogo = document.getElementById('textLogo');
+		if (textLogo) {
+			textLogo.remove();
+		}
+	}
 
+	function openResume() {
+		window.location.href = 'http://127.0.0.1:5173/resume';
+	}
+
+	function openAbout() {
+		window.location.href = 'http://127.0.0.1:5173/about';
+	}
 </script>
 
 <div id="container">
-    <div id="animationLogo"></div>
-        <div id="textLogo">
-            <p>aahland - anton åhlander</p>
-        </div>
+	<div id="animationContainer">
+		<div id="animationLogo" />
+		<div id="textLogo">
+			<button id="enterBtn" on:click={openNav}>aahland - anton åhlander</button>
+		</div>
+	</div>
+
+	<div id="navContainer">
+		<nav>
+			<ul>
+				<button on:click={openResume} class="navBtn" id="resumeBtn">Resume</button>
+				<button on:click={openAbout} class="navBtn" id="aboutBtn">About</button>
+			</ul>
+		</nav>
+	</div>
 </div>
 
 <style>
+	#container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100vh;
+		justify-content: center;
+	}
 
-#container {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	height: 100vh;
-    justify-content: center;
+	#animationContainer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		/* height: 100vh;
+		justify-content: center; */
+	}
 
-}
+	#animationLogo {
+		background: url('/src/lib/yellowhatsprite.png');
+		height: 205px;
+		width: 150px;
+		animation: animateCoder 2s steps(13) infinite;
+	}
 
-#animationLogo {
-    background: url("/src/lib/yellowhatsprite.png");
-    height: 205px;
-    width: 150px;
-    animation: animateCoder 2s steps(13) infinite;
+	@keyframes animateCoder {
+		from {
+			background-position: 1px;
+		}
+		to {
+			background-position: 2000px;
+		}
+	}
+	#enterBtn {
+		border: 2px solid black;
+		width: fit-content;
+		height: 40px;
+		background: white;
+		font-family: monospace;
+		font-size: larger;
+	}
 
+	#enterBtn:hover {
+		background: rgb(252, 177, 190);
+	}
 
-}
+	#navContainer {
+		visibility: hidden;
+	}
 
-@keyframes animateCoder {
-    from 
-    {
-        background-position: 1px;
-    }
-    to {
-        background-position: 2000px;
-    }
-}
-#textLogo {
-    border: 2px solid black;
-    width: fit-content;
-   
-    background: white;
-    font-family: monospace;
-    font-size: larger;
-}
+	ul {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		padding: 0px;
+		margin-top: 0px;
+	}
 
-#textLogo:hover {
-    background: rgb(252, 177, 190);
+	.navBtn {
+		border: 2px solid black;
+		width: fit-content;
+		height: 40px;
+		background: white;
+		font-family: monospace;
+		font-size: larger;
+	}
 
-}
-	
+	.navBtn:hover {
+		background: rgb(252, 177, 190);
+	}
+
+	#aboutBtn {
+		margin-left: 20px;
+		width: 72px;
+	}
+	#resumeBtn {
+		margin-right: 10px;
+		width: 72px;
+		color: black;
+		text-decoration: none;
+	}
 </style>
