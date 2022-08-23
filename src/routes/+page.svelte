1de0b9886a94e266da5a2b5904c8480a199752c9
aahland <ahlandera@hotmail.com>
2022-08-23 08:09:07 +0200

    feat: animated dude added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<div id="container">
+    <div id="animationLogo"></div>
+        <div id="textLogo">
+            <p>aahland - anton åhlander</p>
+        </div>
+</div>
+
+<style>
+
+#container {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	height: 100vh;
+    justify-content: center;
+
+}
+
+#animationLogo {
+    background: url("/src/lib/yellowhatsprite.png");
+    height: 205px;
+    width: 150px;
+    animation: animateCoder 2s steps(13) infinite;
+
+
+}
+
+@keyframes animateCoder {
+    from 
+    {
+        background-position: 1px;
+    }
+    to {
+        background-position: 2000px;
+    }
+}
+#textLogo {
+    border: 2px solid black;
+    width: fit-content;
+   
+    background: white;
+    font-family: monospace;
+    font-size: larger;
+}
+
+#textLogo:hover {
+    background: rgb(252, 177, 190);
+
+}
+	
+</style>
