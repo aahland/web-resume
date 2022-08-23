785038e2910ebbb0d5d674c4846256913e771535
aahland <ahlandera@hotmail.com>
2022-08-23 22:09:47 +0200

    WIP: resume

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { dirty_components } from 'svelte/internal';
+	
 </script>
 
 <div id="resumeContainer">
@@ -9,7 +9,6 @@
 		<p>0709748277</p>
 	</div>
 	<main>
-		<aside />
 		<div id="experienceWrapper">
 			<h2>EXPERIENCE</h2>
 			<div id="experiences">
@@ -17,9 +16,9 @@
 					<div class="ex-top">
 						<h3>Frontend Developer - Coolstuff</h3>
 					</div>
-					<p>01.2022 - Present</p>
+					<p class="years">01.2022 - Present</p>
 
-					<p>
+					<p class="desc">
 						During this 26 weeks long internship at Coolstuff I've been working on both
 						"day-to-day"-tasks related to improving the webpage/web shop and on a larger, standalone
 						project. Working with Javascript, Svelte, TypeScript, graphQL, Jest, Cypress, Version
@@ -30,9 +29,9 @@
 					<div class="ex-top">
 						<h3>Researcher - Mercuri Urval</h3>
 					</div>
-					<p>05.2018 - 07.2020</p>
+					<p class="years">05.2018 - 07.2020</p>
 
-					<p>
+					<p class="desc">
 						Headhunting, Executive Search and Professional Recruitment of managers and specialists
 						in different sectors. Both national and international projects. Last 9 months I have
 						been part of the Global Talent Acquisition Team with focus on Sweden.
@@ -42,9 +41,9 @@
 					<div class="ex-top">
 						<h3>Team Operation Coordinator - Mercuri Urval</h3>
 					</div>
-					<p>11.2017 - 05.2018</p>
+					<p class="years">11.2017 - 05.2018</p>
 
-					<p>
+					<p class="desc">
 						Administrate and coordinate recruitment projects: handle invoices, communicate and
 						arrange meetings, manage receipts and account coding of expense reports, team support.
 					</p>
@@ -53,17 +52,17 @@
 					<div class="ex-top">
 						<h3>Office Manager (interim) - Mercuri Urval</h3>
 					</div>
-					<p>08.2017 - 11.2017</p>
+					<p class="years">08.2017 - 11.2017</p>
 
-					<p>Interim office manager, working with reception, purchase, suppliers etc.</p>
+					<p class="desc">Interim office manager, working with reception, purchase, suppliers etc.</p>
 				</div>
 				<div class="ex" id="experience5">
 					<div class="ex-top">
 						<h3>Staffing and Recruitment Assistant - Academic Work</h3>
 					</div>
-					<p>01.2015 - 08.2016</p>
+					<p class="years">01.2015 - 08.2016</p>
 
-					<p>
+					<p class="desc">
 						Team IT/Tech. Part time job during studies. Full time recruiters in their processes.
 						Advertisement, selection, phone interviews, face-to-face interviews. Last 2-3 months
 						full time.
@@ -73,9 +72,9 @@
 					<div class="ex-top">
 						<h3>ITC - Mercuri Urval</h3>
 					</div>
-					<p>09.2013 - 08.2014</p>
+					<p class="years">09.2013 - 08.2014</p>
 
-					<p>
+					<p class="desc">
 						Responsible for the IT situation at the Sweden offices. Users, personal computers,
 						purchase, networks etc. Worked 3 days a week as an interim.
 					</p>
@@ -84,9 +83,9 @@
 					<div class="ex-top">
 						<h3>Deli/Sales - Ica Gruppen</h3>
 					</div>
-					<p>05.2012 - 04.2014</p>
+					<p class="years">05.2012 - 04.2014</p>
 
-					<p>
+					<p class="desc">
 						Worked in the fresh food department with fish, meats and cheese. I did also work as a
 						cashier.
 					</p>
@@ -100,61 +99,108 @@
 					<div class="ed-left">
 						<h3>Frontend Development</h3>
 						<p>Mediainstitutet</p>
-						<p>09.2020 - 09.2022</p>
+						<p class="years">09.2020 - 09.2022</p>
 					</div>
-					<p>Studying Frontend-Development at Mediainstitutet, graduating September 28th, 2022.</p>
+					<p class="desc">Studying Frontend-Development at Mediainstitutet, graduating September 28th, 2022.</p>
 				</div>
 				<div class="ed" id="education2">
 					<div class="ed-left">
 						<h3>Grafisk Design - Mobila Enheter</h3>
 						<p>Kristianstad University</p>
-						<p>2017</p>
+						<p class="years">2017</p>
 					</div>
 				</div>
 				<div class="ed" id="education3">
 					<div class="ed-left">
 						<h3>Typografi och digital publicering</h3>
 						<p>Linneaus University</p>
-						<p>2017</p>
+						<p class="years">2017</p>
 					</div>
 				</div>
 				<div class="ed" id="education4">
 					<div class="ed-left">
 						<h3>Grafisk Design - Tryck/Print</h3>
 						<p>Kristianstad University</p>
-						<p>2016</p>
+						<p class="years">2016</p>
 					</div>
 				</div>
 				<div class="ed" id="education5">
 					<div class="ed-left">
 						<h3>Grafisk Design - Introduction course</h3>
 						<p>Kristianstad University</p>
-						<p>2016</p>
+						<p class="years">2016</p>
 					</div>
 				</div>
 				<div class="ed" id="education6">
 					<div class="ed-left">
 						<h3>Personalvetenskap / Human Resources</h3>
 						<p>Gothenburg University</p>
-						<p>2014 - 2015</p>
+						<p class="years">2014 - 2015</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	</main>
-	<h2>SKILLS</h2>
-	<h2>SOMETHING</h2>
 	<div id="secondhalf">
+		<div id="skillWrapper">
+		<h2>SKILLS</h2>
 		<div id="skills">
-			<p />
+		 <div class="row">
+			<p>HTML5</p>
+			<p>CSS</p>
+			<p>JavaScript</p>
+			<p>SvelteKit</p>
+			<p>Vue</p>
+			
+		 </div>
+		 <div class="row">
+			
+			<p>React</p>
+			<p>TypeScript</p>
+			<p>npm</p>
+			<p>Jest</p>
+			<p>Unit Test</p>
+			
+			
+			
 		</div>
-		<div id="something">
-			<h2>SOMETHING</h2>
+		<div class="row">
+			<p>Cypress</p>
+			<p>E2E</p>
+			<p>Scrum</p>
+			<p>CLI</p>
+			<p>GitHub</p>
+			
+			
+		</div>
+		<div class="row">
+			<p>Git</p>
+			<p>graphQL</p>
+			<p>Databases</p>
+			<p>CMS</p>
+			<p>SSR</p>
+			
+		</div>
+		<div class="row">
+			
+		</div>
+		</div>
+	    </div>
+		<div id="linksWrapper">
+		<h2>LINKS</h2>
+		<div id="links">
+			<img src="/images/linkedinLogo.png" alt="linkedIn logo">
+			<img src="/images/githublogotype.png" alt="github logo">
+		</div>
 		</div>
 	</div>
 </div>
 
 <style>
+	img {
+		height: 50px;
+	}
+
 	main {
 		display: flex;
 		flex-direction: row;
@@ -164,9 +210,9 @@
 	}
 
 	#experiences {
-		width: 40vw;
+		width: 50vw;
 		text-align: center;
-		height: 41vh;
+		height: 40vh;
 		overflow: scroll;
 	}
 
@@ -198,10 +244,11 @@
 	}
 
 	#educations {
-		width: 40vw;
+		width: 50vw;
 		text-align: center;
-		height: 41vh;
+		height: 40vh;
 		overflow: scroll;
+		overflow-x: hidden;
 	}
 
 	#header {
@@ -214,30 +261,33 @@
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
-		width: 80vw;
-		margin-left: 10vw;
-
+		width: 95vw;
 		background-color: white;
 	}
 
 	#skills {
 		background-color: rgb(87, 162, 162);
-		height: 30vh;
-		width: 40vw;
+		height: 21vh;
+		width:37.5vw;
 		align-self: center;
+		align-items: center;
 		border-radius: 5px;
 		padding: 10px;
-		margin: 10px 10px 0px 10px;
 		color: white;
+		overflow: scroll;
+		display: flex;
+    	flex-direction: column;
 	}
-	#something {
+	#links {
 		background-color: rgb(87, 162, 162);
-		height: 30vh;
-		width: 40vw;
+		height: 21vh;
+		width: 37.5vw;
 		align-self: center;
 		border-radius: 5px;
 		padding: 10px;
 		margin: 10px 10px 0px 10px;
+		overflow: hidden;
+		
 	}
 
 	h1 {
@@ -250,6 +300,7 @@
 		text-align: center;
 		text-decoration: underline;
 		color: rgb(87, 162, 162);
+		margin-top: 2vh
 	}
 
 	h3 {
@@ -261,8 +312,126 @@
 		margin: 5px;
 	}
 
-	aside {
-		background: white;
-		width: 20vw;
+	 #secondhalf p {
+		background-color: rgb(240, 240, 240); 
+		width: fit-content;
+		color: rgb(87, 162, 162);
+		border-radius: 10px;
+		padding: 0px 5px 0px 5px;
+		font-size:  small;
+	} 
+
+	.row {
+		display: flex;
+		flex-direction: row;
+		
+
+
 	}
+
+	@media screen and (min-width: 320px) {
+
+		#secondhalf {
+			flex-direction: column;
+			
+		}
+
+		/* #secondhalf h2{
+			
+		} */
+
+		#secondhalf p {
+			font-size: small;
+		}
+
+		.row {
+			flex-direction: row;
+		}
+
+
+
+		h2 {
+			font-size: large;
+			margin-bottom: 5px;
+		}
+
+		.years {
+			font-size: x-small;
+		}
+
+		.desc {
+			font-size: small;
+		}
+
+		#skills {
+			overflow: visible;
+			width: 93%;
+			align-content: center;
+			height: fit-content;
+			margin-left: 7px;
+			font-weight: 400;
+			
+		}
+
+		#links {
+			width: 93%;
+			height: fit-content;
+			margin: 10px 10px 0px 5px;
+			display: flex;
+   			justify-content: center;
+		}
+
+		
+}
+
+@media screen and (min-width: 400px) {
+		#secondhalf p {
+			background-color: white;
+			font-size: medium;
+		}
+
+		#skills {
+			width: 96%;
+		}
+
+		#links {
+			width: 96%;
+		}
+
+
+		.years {
+			font-size: small;
+		}
+
+		.desc {
+			font-size: medium;
+		}
+
+		.row {
+			flex-direction: row;
+		}
+
+		
+
+		
+	
+}
+
+@media screen and (min-width: 600px) {
+		#secondhalf p {
+			background-color: white;
+			font-size: large;
+		}
+
+		#skills {
+			width: 99%;
+		}
+
+		#links {
+			width: 99%;
+		}
+		
+	
+}
+
 </style>
