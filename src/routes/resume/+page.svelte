452d78df705c9f3540bd22e7960139f352aea85d
aahland <ahlandera@hotmail.com>
2022-08-28 13:40:50 +0200

    refactor: change html to js + svelte solution

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,141 +1,49 @@
 <script>
-</script>
 
+import experiences from "../../lib/experienceData.json";
+import education from "../../lib/educationData.json";
+console.log(experiences)
+console.log(education)
+
+</script>
 <div id="resumeContainer">
 	<div id="header">
 		<h1>ANTON ÅHLANDER</h1>
 		<p id="email">ahlandera@hotmail.com</p>
 		<p id="phonenumber">0709748277</p>
 	</div>
+
 	<div class="maincontent">
 		<div id="experienceWrapper">
 			<h2>EXPERIENCE</h2>
-			<div id="experiences">
-				<div class="ex" id="experience1">
-					<div class="ex-top">
-						<h3>Frontend Developer</h3>
-						<h3>Coolstuff</h3>
-					</div>
-					<p class="years">01.2022 - Present</p>
-					<p class="desc">
-						A 26 weeks long internship at Coolstuff where I've been working on "day-to-day" tasks
-						like writing new features, bug fixes, style tasks etc. And also on a larger, standalone
-						project. Working with Javascript, Svelte, TypeScript, graphQL, Jest, Cypress, version
-						handling with Git/Github.
-					</p>
+				<div id="experiences">
+					{#each experiences.experiences as experience, i }
+						<div class="ex" id="experience{i}">
+							<div class="ex-top">
+								<h3>{experience.title}</h3>
+								<h3>{experience.org}</h3>
+							</div>
+							<p class="years">{experience.dates}</p>
+							<p class="desc">{experience.desc}</p>
+						</div>
+					{/each}
 				</div>
-				<div class="ex" id="experience2">
-					<div class="ex-top">
-						<h3>Researcher - Mercuri Urval</h3>
-					</div>
-					<p class="years">05.2018 - 07.2020</p>
-					<p class="desc">
-						Headhunting, Executive Search and Professional Recruitment of managers and specialists
-						in different sectors. Both national and international projects. Last 9 months I have
-						been part of the Global Talent Acquisition Team with focus on Sweden.
-					</p>
-				</div>
-				<div class="ex" id="experience3">
-					<div class="ex-top">
-						<h3>Team Operation Coordinator - Mercuri Urval</h3>
-					</div>
-					<p class="years">11.2017 - 05.2018</p>
-					<p class="desc">
-						Administrate and coordinate recruitment projects: handle invoices, communicate and
-						arrange meetings, manage receipts and account coding of expense reports, team support.
-					</p>
-				</div>
-				<div class="ex" id="experience4">
-					<div class="ex-top">
-						<h3>Office Manager (interim) - Mercuri Urval</h3>
-					</div>
-					<p class="years">08.2017 - 11.2017</p>
-					<p class="desc">
-						Interim office manager, working with reception, purchase, suppliers etc.
-					</p>
-				</div>
-				<div class="ex" id="experience5">
-					<div class="ex-top">
-						<h3>Staffing and Recruitment Assistant - Academic Work</h3>
-					</div>
-					<p class="years">01.2015 - 08.2016</p>
-					<p class="desc">
-						Team IT/Tech. Part time job during studies. Full time recruiters in their processes.
-						Advertisement, selection, phone interviews, face-to-face interviews. Last 2-3 months
-						full time.
-					</p>
-				</div>
-				<div class="ex" id="experience6">
-					<div class="ex-top">
-						<h3>ITC - Mercuri Urval</h3>
-					</div>
-					<p class="years">09.2013 - 08.2014</p>
-					<p class="desc">
-						Responsible for the IT situation at the Sweden offices. Users, personal computers,
-						purchase, networks etc. Worked 3 days a week as an interim.
-					</p>
-				</div>
-				<div class="ex" id="experience7">
-					<div class="ex-top">
-						<h3>Deli/Sales - Ica Gruppen</h3>
-					</div>
-					<p class="years">05.2012 - 04.2014</p>
-					<p class="desc">
-						Worked in the fresh food department with fish, meats and cheese. I did also work as a
-						cashier.
-					</p>
-				</div>
-			</div>
 		</div>
+
 		<div id="educationWrapper">
 			<h2>EDUCATION</h2>
-			<div id="educations">
-				<div class="ed" id="education1">
-					<div class="ed-left">
-						<h3>Frontend Development</h3>
-						<p class="school">Mediainstitutet</p>
-						<p class="years">09.2020 - 09.2022</p>
-					</div>
-					<p class="desc">
-						Studying Frontend-Development at Mediainstitutet, graduating September 28th, 2022.
-					</p>
+				<div id="educations">
+					{#each education.education as education }
+						<div class="ed" id="education1">
+							<div class="ed-left">
+								<h3>{education.title}</h3>
+								<h3 class="school">{education.school}</h3>
+							</div>
+							<p class="years">{education.dates}</p>
+							<p class="desc">{education.desc}</p>
+						</div>
+					{/each}
 				</div>
-				<div class="ed" id="education2">
-					<div class="ed-left">
-						<h3>Graphic Design - Mobile devices</h3>
-						<p class="school">Kristianstad University</p>
-						<p class="years">2017</p>
-					</div>
-				</div>
-				<div class="ed" id="education3">
-					<div class="ed-left">
-						<h3>Typography & digital publishing</h3>
-						<p class="school">Linneaus University</p>
-						<p class="years">2017</p>
-					</div>
-				</div>
-				<div class="ed" id="education4">
-					<div class="ed-left">
-						<h3>Graphic Design - Printed material</h3>
-						<p class="school">Kristianstad University</p>
-						<p class="years">2016</p>
-					</div>
-				</div>
-				<div class="ed" id="education5">
-					<div class="ed-left">
-						<h3>Graphic Design - Introduction course</h3>
-						<p class="school">Kristianstad University</p>
-						<p class="years">2016</p>
-					</div>
-				</div>
-				<div class="ed" id="education6">
-					<div class="ed-left">
-						<h3>Personalvetenskap / Human Resources</h3>
-						<p class="school">Gothenburg University</p>
-						<p class="years">2014 - 2015</p>
-					</div>
-				</div>
-			</div>
 		</div>
 	</div>
 	<div id="secondhalf">
@@ -168,329 +76,341 @@
 		<div id="linkswrapper">
 			<div id="links">
 				<div id="linkedinContainer">
-					<img id="linkedin" src="/images/linkedinLogo.png" alt="linkedIn logo" />
+					<a href="https://www.linkedin.com/in/anton-%C3%A5hlander-038a3a42/"><img id="linkedin" src="/images/linkedinLogo.png" alt="linkedIn logo" /></a>
 				</div>
-				<img id="github" src="/images/githublogotype.png" alt="github logo" />
+				<a href="https://github.com/aahland"><img id="github" src="/images/githublogotype.png" alt="github logo" /></a>
 			</div>
 		</div>
 	</div>
 </div>
 
-<style>
-	img {
-		height: 80px;
-		-webkit-filter: invert(100%);
-		filter: invert(100%);
-	}
-
-	#github {
-		height: 85px;
-	}
-
-	#linkedinContainer {
-		width: 90px;
-	}
-
-	.maincontent {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		width: 100%;
-		background-color: white;
-		align-items: center;
-	}
-
-	::-webkit-scrollbar {
-		width: 5px;
-	}
-
-	::-webkit-scrollbar-track {
-		background-color: white;
-	}
-
-	::-webkit-scrollbar-thumb {
-		background: rgb(87, 162, 162);
-		border-radius: 10px;
-		height: 1px;
-		width: 1px;
-	}
-
-	.ex {
-		text-align: left;
-		background-color: white;
-		border-radius: 5px;
-		padding: 10px;
-		margin: 10px 10px 0px 10px;
-		color: rgb(240, 240, 240);
-		background-color: rgb(87, 162, 162);
-		font-size: large;
-	}
-
-	.ex-top {
-		display: flex;
-		flex-direction: column;
-		font-size: larger;
-		margin-top: 0px;
-	}
-
-	.ed-left {
-		font-size: larger;
-	}
-
-	.ed {
-		text-align: left;
-		align-content: center;
-		align-items: center;
-		border-radius: 5px;
-		padding: 10px;
-		margin: 10px 10px 0px 10px;
-		background-color: rgb(87, 162, 162);
-		color: rgb(240, 240, 240);
-		font-size: larger;
-	}
-
-	#skillsdiv {
-		text-align: left;
-		align-content: center;
-		align-items: center;
-		border-radius: 5px;
-		padding: 0px;
-		margin: 10px 10px 0px 10px;
-		background-color: rgb(87, 162, 162);
-		color: rgb(240, 240, 240);
-		font-size: larger;
-	}
-
-	#experiences {
-		max-width: 450px;
-		text-align: center;
-		height: 300px;
-		overflow: scroll;
-	}
-
-	#educations {
-		max-width: 450px;
-		text-align: center;
-		height: 300px;
-		overflow: scroll;
-	}
-
-	#header {
-		background: white;
-		align-items: center;
-		text-align: center;
-	}
-
-	#secondhalf {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		background-color: white;
-		gap: 10px;
-		width: 100%;
-		max-width: 900px;
-	}
-
-	#secondhalf h2 {
-		margin: 0px;
-	}
-
-	#skills {
-		text-align: center;
-		max-height: 150px;
-		overflow: hidden;
-	}
-
-	#linkswrapper {
-		text-align: center;
-		height: 300px;
-	}
-
-	#links {
-		max-width: 800px;
-		margin: 0 auto;
-		display: flex;
-		gap: 1rem;
-		grid-template-columns: repeat(4, 1fr);
-		justify-content: center;
-		align-content: center;
-		flex-wrap: wrap;
-		background-color: rgb(87, 162, 162);
-		border-radius: 5px;
-		text-align: center;
-		margin: 10px 10px 0px 10px;
-		padding: 50px 100px 50px 100px;
-	}
-
-	#phonenumber {
-		margin: 0px;
-	}
-
-	#email {
-		margin-bottom: 2px;
-		margin-top: 1px;
-	}
-
-	h1 {
-		color: rgb(87, 162, 162);
-		font-size: 50px;
-		margin-bottom: 0px;
-	}
-
-	h2 {
-		text-align: center;
-		text-decoration: underline;
-		color: rgb(87, 162, 162);
-		margin-top: 2vh;
-		margin-bottom: 0px;
-		font-size: xx-large;
-	}
-
-	h3 {
-		padding: 0px;
-		margin: 0px 0px 0px 0px;
-	}
-
-	.skillgroup {
-		max-width: 800px;
-		margin: 0 auto;
-		display: flex;
-		gap: 1rem;
-		grid-template-columns: repeat(4, 1fr);
-		justify-content: center;
-		align-content: center;
-		flex-wrap: wrap;
-	}
-
-	.skillgroup p {
-		background: white;
-		color: rgb(87, 162, 162);
-		border-radius: 10px;
-		padding: 3px 10px 3px 10px;
-	}
-
-	.skill {
-		font-weight: bolder;
-		font-size: large;
-	}
-
-	#resumeContainer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-content: center;
-		align-items: center;
-		font-family: monospace;
-	}
-
-	.descHack {
-		color: rgb(87, 162, 162);
-	}
-
-	@media screen and (max-width: 450px) {
-		.ed {
-			font-size: small;
+	<style>
+		img {
+			height: 80px;
+			-webkit-filter: invert(100%);
+			filter: invert(100%);
 		}
-		.ex {
-			font-size: small;
+	
+		#github {
+			height: 85px;
 		}
-
+	
+		#linkedinContainer {
+			width: 90px;
+		}
+	
 		.maincontent {
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			width: 100%;
+			background-color: white;
+			align-items: center;
+		}
+	
+		::-webkit-scrollbar {
+			width: 5px;
+		}
+	
+		::-webkit-scrollbar-track {
+			background-color: white;
+		}
+	
+		::-webkit-scrollbar-thumb {
+			background: rgb(87, 162, 162);
+			border-radius: 10px;
+			height: 1px;
+			width: 1px;
+		}
+	
+		.ex {
+			text-align: left;
+			background-color: white;
+			border-radius: 5px;
+			padding: 10px;
+			margin: 10px 10px 0px 10px;
+			color: rgb(240, 240, 240);
+			background-color: rgb(87, 162, 162);
+			font-size: large;
+		}
+	
+		.ex-top {
+			display: flex;
 			flex-direction: column;
+			font-size: larger;
+			margin-top: 0px;
 		}
-
+	
+		.ed-left {
+			font-size: larger;
+		}
+	
+		.ed {
+			text-align: left;
+			align-content: center;
+			align-items: center;
+			border-radius: 5px;
+			padding: 10px;
+			margin: 10px 10px 0px 10px;
+			background-color: rgb(87, 162, 162);
+			color: rgb(240, 240, 240);
+			font-size: larger;
+		}
+	
+		#skillsdiv {
+			text-align: left;
+			align-content: center;
+			align-items: center;
+			border-radius: 5px;
+			padding: 0px;
+			margin: 10px 10px 0px 10px;
+			background-color: rgb(87, 162, 162);
+			color: rgb(240, 240, 240);
+			font-size: larger;
+		}
+	
+		#experiences {
+			max-width: 450px;
+			text-align: center;
+			height: 300px;
+			overflow: scroll;
+		}
+	
+		#educations {
+			max-width: 450px;
+			text-align: center;
+			height: 300px;
+			overflow: scroll;
+		}
+	
+		#header {
+			background: white;
+			align-items: center;
+			text-align: center;
+		}
+	
+		#secondhalf {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			background-color: white;
+			gap: 10px;
+			width: 100%;
+			max-width: 900px;
+		}
+	
+		#secondhalf h2 {
+			margin: 0px;
+		}
+	
+		#skills {
+			text-align: center;
+			max-height: 150px;
+			overflow: hidden;
+		}
+	
+		#linkswrapper {
+			text-align: center;
+			height: 300px;
+		}
+	
+		#links {
+			max-width: 800px;
+			margin: 0 auto;
+			display: flex;
+			gap: 1rem;
+			grid-template-columns: repeat(4, 1fr);
+			justify-content: center;
+			align-content: center;
+			flex-wrap: wrap;
+			background-color: rgb(87, 162, 162);
+			border-radius: 5px;
+			text-align: center;
+			margin: 10px 10px 0px 10px;
+			padding: 50px 100px 50px 100px;
+		}
+	
+		#phonenumber {
+			margin: 0px;
+		}
+	
+		#email {
+			margin-bottom: 2px;
+			margin-top: 1px;
+		}
+	
+		h1 {
+			color: rgb(87, 162, 162);
+			font-size: 50px;
+			margin-bottom: 0px;
+		}
+	
 		h2 {
-			margin-bottom: 10px;
+			text-align: center;
+			text-decoration: underline;
+			color: rgb(87, 162, 162);
+			margin-top: 2vh;
+			margin-bottom: 0px;
+			font-size: xx-large;
 		}
-
+	
+		h3 {
+			padding: 0px;
+			margin: 0px 0px 0px 0px;
+		}
+	
+		.skillgroup {
+			max-width: 800px;
+			margin: 0 auto;
+			display: flex;
+			gap: 1rem;
+			grid-template-columns: repeat(4, 1fr);
+			justify-content: center;
+			align-content: center;
+			flex-wrap: wrap;
+		}
+	
+		.skillgroup p {
+			background: white;
+			color: rgb(87, 162, 162);
+			border-radius: 10px;
+			padding: 3px 10px 3px 10px;
+		}
+	
 		.skill {
 			font-weight: bolder;
-			font-size: medium;
+			font-size: large;
+		}
+	
+		#resumeContainer {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-content: center;
+			align-items: center;
+			font-family: monospace;
+		}
+	
+		.descHack {
+			color: rgb(87, 162, 162);
 		}
 
 		#skills {
-			overflow: scroll;
+			padding: 5px;
 		}
+	
+		@media screen and (max-width: 450px) {
+			.ed {
+				font-size: small;
+			}
+			.ex {
+				font-size: small;
+			}
+	
+			.maincontent {
+				flex-direction: column;
+			}
+	
+			h2 {
+				margin-bottom: 10px;
+			}
+	
+			.skill {
+				font-weight: bolder;
+				font-size: medium;
+			}
+	
+			#skills {
+				max-height: none;
+			}
+	
+			#secondhalf h2 {
+				margin-bottom: 10px;
+			}
 
-		#secondhalf h2 {
-			margin-bottom: 10px;
+			#skillRooms {
+				height: fit-content;
+			}
 		}
-	}
+	
+		@media screen and (max-width: 600px) {
+			.ed {
+				font-size: medium;
+			}
+	
+			.ex {
+				font-size: medium;
+			}
+	
+			.ex-top {
+				font-size: large;
+			}
+	
+			h2 {
+				font-size: x-large;
+				margin-bottom: 10px;
+			}
+	
+			#secondhalf h2 {
+				font-size: x-large;
+				margin-bottom: 10px;
+			}
 
-	@media screen and (max-width: 600px) {
-		.ed {
-			font-size: medium;
+			#skills {
+				overflow: scroll;
+			}
 		}
-
-		.ex {
-			font-size: medium;
+	
+		@media screen and (max-width: 900px) {
+			.ed {
+				font-size: large;
+			}
+	
+			.ex {
+				font-size: large;
+			}
+	
+			.ex-top {
+				font-size: larger;
+			}
+	
+			.years {
+				font-size: small;
+			}
+	
+			.school {
+				margin: 0px 0px 0px 0px;
+				font-weight: bold;
+			}
+	
+			h2 {
+				font-size: xx-large;
+				margin-bottom: 10px;
+			}
+	
+			#secondhalf h2 {
+				font-size: xx-large;
+				margin-bottom: 10px;
+			}
 		}
-
-		.ex-top {
-			font-size: large;
+	
+		@media screen and (max-width: 1440px) {
+			h2 {
+				margin-bottom: 15px;
+			}
+	
+			#secondhalf h2 {
+				margin-bottom: 0px;
+			}
 		}
-
-		h2 {
-			font-size: x-large;
-			margin-bottom: 10px;
+	
+		@media screen and (max-width: 2040px) {
+			h2 {
+				margin-bottom: 15px;
+			}
+	
+			#secondhalf h2 {
+				margin-bottom: 0px;
+			}
 		}
-
-		#secondhalf h2 {
-			font-size: x-large;
-			margin-bottom: 10px;
-		}
-	}
-
-	@media screen and (max-width: 900px) {
-		.ed {
-			font-size: large;
-		}
-
-		.ex {
-			font-size: large;
-		}
-
-		.ex-top {
-			font-size: larger;
-		}
-
-		.years {
-			font-size: small;
-		}
-
-		.school {
-			margin: 0px 0px 0px 0px;
-			font-weight: bold;
-		}
-
-		h2 {
-			font-size: xx-large;
-			margin-bottom: 10px;
-		}
-
-		#secondhalf h2 {
-			font-size: xx-large;
-			margin-bottom: 10px;
-		}
-	}
-
-	@media screen and (max-width: 1440px) {
-		h2 {
-			margin-bottom: 15px;
-		}
-
-		#secondhalf h2 {
-			margin-bottom: 10px;
-		}
-	}
-
-	@media screen and (max-width: 2040px) {
-		h2 {
-			margin-bottom: 15px;
-		}
-
-		#secondhalf h2 {
-			margin-bottom: 10px;
-		}
-	}
-</style>
+	</style>
