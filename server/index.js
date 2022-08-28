f8fae9315cf2af4002ec4bb4635382dc2d0cd6cf
aahland <ahlandera@hotmail.com>
2022-08-28 14:51:46 +0200

    deploy: changes to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const path = require('path');
+import path from 'express';
 const port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.get('*', (req, res) => {
