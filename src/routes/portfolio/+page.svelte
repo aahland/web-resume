ab4818c440d858baca482edd5d610a16497f81d2
aahland <anton.ahlander@coolstuff.com>
2022-09-07 16:25:29 +0200

    style: add style to portfolio

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,37 +1,42 @@
 <script>
+	import '../../../src/app.css';
 </script>
 
 <div id="wrapper">
 	<div id="header">
 		<h1>Portfolio</h1>
-		<p>
-			Welcome to my portfolio! Below I have listed a few projects that I have done in the year of
-			2022
-		</p>
 	</div>
 	<div id="portfolioContent">
 		<div id="vadFanSka">
 			<div class="textAboutProject">
 				<h2>Vadfanskajagköpa.se</h2>
 				<p class="description">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque, dolores magni, eligendi
-					autem corporis harum omnis quia explicabo laboriosam nemo, fugiat dolore maxime doloremque
-					sequi optio architecto eum veniam tempore!
+					Vadfanskajagköpa.se is a stand-alone project built by me and my internship colleague at
+					Coolstuff. The site is aiming to present random coolstuff products with a short and fun
+					description to drive traffic to coolstuff's webshop. The project was written with the
+					latest SvelteKit and is hosted on Cloudflare using edge computing and cloudflare workers.
+					The project is hooked up to a headless CMS and is using BasicAuth for authification.
 				</p>
 				<a href="www.vadfanskajagkopa.se">www.vadfanskajagkopa.se</a>
 				<a href="github.com">github-link-if-possible</a>
 			</div>
 			<div class="imageWrapper">
-				<img src="images/vadfanskaMockup.png" alt="mobile and desktop view of application" />
+				<img
+					id="vadfanMockup"
+					src="images/vadfanskaMockup.png"
+					alt="mobile and desktop view of application"
+				/>
 			</div>
 		</div>
 		<div id="lyricalGenius">
 			<div class="textAboutProject">
 				<h2>Lyrical Genius</h2>
 				<p class="description">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque, dolores magni, eligendi
-					autem corporis harum omnis quia explicabo laboriosam nemo, fugiat dolore maxime doloremque
-					sequi optio architecto eum veniam tempore!
+					Lyrical Genius is a realtime multiplayer game played in the browser. The goal of the game
+					is to match a snippet of a song lyric to the correct song. In other words, a music quiz
+					where song lyrics are the essential part of the question. The project was written with
+					SvelteKit and uses socket.io for the multiplayer functionality. This project was my final
+					project in school but has been updated since.
 				</p>
 				<a href="https://quiz-lyrical-genius.herokuapp.com"
 					>https://quiz-lyrical-genius.herokuapp.com/</a
@@ -42,6 +47,7 @@
 			</div>
 			<div class="imageWrapper">
 				<img
+					id="lyricalMockup"
 					src="images/lyricalgeniusquizMockup.png"
 					alt="mobile and desktop view of application"
 				/>
@@ -49,22 +55,18 @@
 		</div>
 	</div>
 </div>
+<footer>
+	<div id="linksContainer">
+		<button><a href="https://web-resume-775.pages.dev">Home</a></button>
+		<button><a href="https://web-resume-775.pages.dev/resume">Resume</a></button>
+		<button><a href="https://web-resume-775.pages.dev/about">About</a></button>
+		<button><a href="https://github.com/aahland">Github</a></button>
+	</div>
+</footer>
 
 <style>
-	@font-face {
-		font-family: 'Montserrat';
-		font-style: normal;
-		font-weight: 300;
-		src: local('Montserrat'),
-			url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
-	}
-
 	#wrapper {
 		background-color: rgb(87, 162, 162);
-	}
-
-	.description {
-		font-weight: 200;
 	}
 
 	#header {
@@ -75,13 +77,13 @@
 		margin: 0px;
 		z-index: 5;
 		padding-top: 20px;
-		padding-left: 20px;
+		padding-left: 5px;
 	}
 
-	#header p {
-		margin-top: 0px;
-		font-size: x-small;
-		width: 85%;
+	.description {
+		text-align: left;
+		font-size: small;
+		font-weight: 200;
 	}
 
 	.textAboutProject {
@@ -89,11 +91,20 @@
 		flex-direction: column;
 		margin-top: -30px;
 		max-width: 80%;
-		font-family: sans-serif;
+		font-family: Montserrat, sans-serif;
 	}
 
 	img {
-		height: 250px;
+		height: 200px;
+		margin-bottom: 15px;
+	}
+
+	#vadfanMockup {
+		margin-top: 35px;
+	}
+
+	#lyricalMockup {
+		margin-top: 30px;
 	}
 
 	.imageWrapper {
@@ -104,6 +115,8 @@
 		background: white;
 		z-index: 10;
 		border-top-right-radius: 100px;
+		display: flex;
+		flex-direction: column;
 	}
 
 	#vadFanSka {
@@ -111,7 +124,6 @@
 		flex-direction: column-reverse;
 		align-items: center;
 		text-align: center;
-		padding-top: 30px;
 	}
 
 	#lyricalGenius {
@@ -123,34 +135,95 @@
 
 	h2 {
 		margin: 0px;
-		font-family: sans-serif;
-		font-weight: 200;
+		font-weight: 400;
+		font-size: large;
+		color: rgb(87, 162, 162);
 	}
 
 	h1 {
-		font-size: 40px;
+		font-size: 50px;
 		margin-bottom: 0px;
-		font-size: 80px;
-		margin-top: 35px;
+		margin-top: 60px;
 		letter-spacing: 5px;
+		font-weight: bold;
 	}
 
 	a {
 		margin-bottom: 5px;
-		font-size: small;
-		color: black;
+		font-size: x-small;
+		text-align: left;
+		color: rgb(80, 80, 80);
+	}
+
+	footer {
+		background: rgb(87, 162, 162);
+		height: 50px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+		align-items: center;
+		margin-top: 20px;
+	}
+
+	#linksContainer {
+		display: flex;
+		flex-direction: row;
+		gap: 30px;
+	}
+
+	button {
+		height: fit-content;
+		background-color: white;
+		border: none;
+		border-radius: 5px;
+	}
+
+	button:hover {
+		height: 30px;
+	}
+
+	button a {
+		text-decoration: none;
+		color: rgb(87, 162, 162);
+		font-weight: bold;
+		font-family: Montserrat, sans-serif;
+		font-size: medium;
+	}
+
+	@media screen and (min-width: 350px) {
+		h1 {
+			font-size: 60px;
+			margin-top: 55px;
+		}
+	}
+
+	@media screen and (min-width: 450px) {
+		h1 {
+			font-size: 80px;
+			margin-top: 40px;
+		}
 	}
 
 	@media screen and (min-width: 625px) {
 		#vadFanSka {
 			flex-direction: row;
-			width: 95%;
+			justify-content: space-around;
+			width: 100%;
+		}
+
+		a {
+			font-size: 12px;
 		}
 
 		#lyricalGenius {
 			margin-top: 50px;
 			flex-direction: row;
-			width: 95%;
+			justify-content: space-around;
+			width: 100%;
+		}
+
+		img {
+			height: 250px;
 		}
 
 		.textAboutProject {
@@ -160,11 +233,12 @@
 
 		#portfolioContent {
 			padding-top: 50px;
-			padding-left: 20px;
+			padding-left: 10px;
+			align-items: center;
 		}
 
 		#header {
-			padding-left: 30px;
+			padding-left: 20px;
 			height: 150px;
 		}
 
@@ -173,6 +247,10 @@
 			margin-top: 55px;
 			letter-spacing: 5px;
 			margin-right: 0px;
+		}
+
+		button a {
+			font-size: medium;
 		}
 	}
 
@@ -183,20 +261,87 @@
 		}
 
 		#vadFanSka {
-			max-width: 800px;
+			align-items: center;
+			max-width: 90%;
 		}
 
 		#lyricalGenius {
-			max-width: 800px;
+			align-items: center;
+			max-width: 90%;
+		}
+
+		img {
+			height: 300px;
+		}
+
+		.textAboutProject {
+			max-width: 600px;
+		}
+
+		.description {
+			font-size: medium;
+		}
+
+		h2 {
+			font-size: x-large;
+			font-weight: 400;
 		}
 
 		#portfolioContent {
 			display: flex;
 			flex-direction: column;
-			align-items: center;
 			align-content: center;
 			justify-content: space-around;
 			margin-right: 0px;
+		}
+
+		a {
+			font-size: 15px;
+		}
+
+		button a {
+			font-size: large;
+		}
+	}
+
+	@media screen and (min-width: 1440px) {
+		#vadFanSka {
+			max-width: 1200px;
+			justify-content: space-between;
+		}
+
+		#lyricalGenius {
+			max-width: 1200px;
+			justify-content: space-between;
+		}
+
+		.textAboutProject {
+			max-width: 700px;
+		}
+
+		.description {
+			font-size: larger;
+		}
+
+		img {
+			height: 380px;
+		}
+
+		h2 {
+			font-weight: 600;
+		}
+
+		h1 {
+			font-size: 200px;
+			margin-top: -10px;
+		}
+
+		button {
+			height: fit-content;
+		}
+
+		button a {
+			font-size: larger;
 		}
 	}
 </style>
